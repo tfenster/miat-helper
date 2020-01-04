ce8fb4ff8d994a74fb68173b0e168db9fd26588c
Tobias Fenster <tobias.fenster@cosmoconsult.com>
2020-01-04 17:46:27 +0100

    add versioning

--- client/scripts/release.bat
+++ client/scripts/release.bat
@@ -1,3 +1,3 @@
 cd app
-dotnet publish "client.csproj" -c Release -r win-x64 --self-contained true /property:PublishSingleFile=true /property:PublishTrimmed=true -o /app/publish
+dotnet publish "client.csproj" -c Release -r win-x64 --self-contained true /property:PublishTrimmed=true /property:PublishSingleFile=true /property:Version=%1 -o /app/publish
 exit
